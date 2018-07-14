da05e34db1d82a24c241e66b6f93f43c153619e7
Unknown <jmesias3@gmail.com>
2018-07-14 12:19:36 -0700

    adds alert

---
+++ ecommerce.js
@@ -0,0 +1,19 @@
+// display
+function onLoadFunction() {
+    show("#loadAlert");
+}
+
+
+//modal alert
+$("#modalSubmit").click(function(){
+
+    // Get input value and save to variable
+    var Name = $("#fullName").val();
+    
+    // 
+    if (Name === "") {
+
+        
+    }
+
+});
